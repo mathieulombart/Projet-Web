ed3236218a7522edb81dc8e59dc365e531b48351
Jean-Baptiste0 <jeanbaptiste.kohldrab@viacesi.fr>
2026-03-09 10:46:57 +0100

    mise a jour

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Charger Twig
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader);
+
+// Page demandée
+$page = $_GET['page'] ?? 'accueil';
+
+// Pages autorisées (noms EXACTS des fichiers twig sans extension)
+$allowedPages = [
+    'accueil',
+    'profil',
+    'wishlist',
+    'offres_postulees',
+    'entreprise'
+];
+
+// Si la page n'existe pas → accueil
+if (!in_array($page, $allowedPages)) {
+    $page = 'accueil';
+}
+
+// Afficher la page
+echo $twig->render($page . '.html.twig');
