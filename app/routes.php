a55f875c77345ade2d2b3910cb0a1fbfca340d76
DorianNZAHOU <dorian.alzac@gmail.com>
2026-03-29 22:04:25 +0200

    whislist fonctionnel

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,7 @@ use App\Application\Controller\EntrepriseController;
 use App\Application\Controller\HomeController;
 use App\Application\Controller\OffreController;
 use App\Application\Controller\ProfilController;
+use App\Application\Controller\WishlistController; // Ajouté
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -20,58 +21,101 @@ return function (App $app) {
         return $response;
     });
 
-
     $app->get('/', [HomeController::class, 'home']);
 
-    
+    // --- ENTREPRISES ---
     $app->get('/entreprises[/{page:\d+}]', [EntrepriseController::class, 'liste'])
         ->setName('liste-entreprises');
+    
     $app->get('/ajout-entreprise', [EntrepriseController::class, 'ajoute'])
         ->setName('ajout-entreprise')
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
+    
     $app->post('/ajout-entreprise', [EntrepriseController::class, 'ajoute'])
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
+    
     $app->get('/modifier-entreprise/{id:\d+}', [EntrepriseController::class, 'modifier'])
         ->setName('modifier-entreprise')
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
+    
     $app->post('/modifier-entreprise/{id:\d+}', [EntrepriseController::class, 'modifier'])
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
+    
     $app->post('/supprimer-entreprise/{id:\d+}', [EntrepriseController::class, 'supprimer'])
         ->setName('supprimer-entreprise')
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
-   
-    $app->get('/offres[/{page:\d+}]', [OffreController::class, 'liste'])->setName('liste-offres');
-    $app->get('/offre/{id:\d+}', [OffreController::class, 'detail'])->setName('detail-offre');
 
-   
-    $app->get('/entreprise/{id:\d+}/offres', [EntrepriseController::class, 'offres'])->setName('entreprise-offres');
+    // --- OFFRES ---
+    $app->get('/offres[/{page:\d+}]', [OffreController::class, 'liste'])
+        ->setName('liste-offres'); // C'est ce nom qu'on utilise dans url_for
+    
+    $app->get('/offre/{id:\d+}', [OffreController::class, 'detail'])
+        ->setName('detail-offre');
 
+    $app->get('/entreprise/{id:\d+}/offres', [EntrepriseController::class, 'offres'])
+        ->setName('entreprise-offres');
 
+    $app->get('/ajout-offre', [OffreController::class, 'ajoute'])
+        ->setName('ajout-offre')
+        ->add(new RoleMiddleware(['admin'],['pilote']))
+        ->add(new AuthMiddleware());
+
+    $app->post('/ajout-offre', [OffreController::class, 'ajoute'])
+        ->add(new RoleMiddleware(['admin'],['pilote']))
+        ->add(new AuthMiddleware());
+
+    $app->post('/offre/supprimer/{id:\d+}', [OffreController::class, 'supprimer'])
+        ->setName('offre-supprimer')
+        ->add(new RoleMiddleware(['admin'],['pilote']))
+        ->add(new AuthMiddleware());
+
+    // --- WISHLIST (Utilise maintenant le WishlistController) ---
+    $app->get('/wishlist', [WishlistController::class, 'index'])
+        ->setName('wishlist')
+        ->add(new AuthMiddleware());
+
+    $app->post('/wishlist/ajouter', [WishlistController::class, 'ajouter'])
+        ->setName('wishlist-ajouter')
+        ->add(new AuthMiddleware());
+
+    $app->post('/wishlist/supprimer/{id:\d+}', [WishlistController::class, 'supprimer'])
+        ->setName('wishlist-supprimer')
+        ->add(new AuthMiddleware());
+
+    // --- AUTH & PROFIL ---
     $app->get('/inscription', function (Request $request, Response $response) {
-        return Twig::fromRequest($request)->render($response, 'inscription.html.twig', []);
+            return Twig::fromRequest($request)->render($response, 'inscription.html.twig', []);
         })
         ->setName('inscription')
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
+
     $app->post('/inscription', [AuthController::class, 'inscription'])
         ->add(new RoleMiddleware(['admin'],['pilote']))
         ->add(new AuthMiddleware());
 
-    
-
-    $app->get('/profil', [ProfilController::class, 'index'])->setName('profil')
+    $app->get('/profil', [ProfilController::class, 'index'])
+        ->setName('profil')
         ->add(new AuthMiddleware());
 
+    $app->get('/connexion', [AuthController::class, 'connexion'])->setName('connexion');
+    $app->post('/connexion', [AuthController::class, 'connexion']);
+    $app->get('/deconnexion', [AuthController::class, 'deconnexion'])->setName('deconnexion');
+
+    // --- CANDIDATURES ---
     $app->get('/postuler/{id:\d+}', [CandidatureController::class, 'formulaire'])->setName('postuler');
     $app->post('/postuler/{id:\d+}', [CandidatureController::class, 'postuler']);
     $app->post('/candidature/retirer/{id:\d+}', [CandidatureController::class, 'retirer'])->setName('candidature-retirer');
+    $app->get('/offres-postulees', [ProfilController::class, 'offresPostulees'])
+        ->setName('offres-postulees')
+        ->add(new AuthMiddleware());
 
-  
+    // --- PAGES STATIQUES ---
     $app->get('/contact', function (Request $request, Response $response) {
         return Twig::fromRequest($request)->render($response, 'contact.html.twig', []);
     })->setName('app_contact');
@@ -84,83 +128,7 @@ return function (App $app) {
         return Twig::fromRequest($request)->render($response, 'confidentialité.html.twig', []);
     })->setName('app_confidentialité');
 
-    
-    $app->get('/wishlist', [ProfilController::class, 'wishlist'])
-        ->setName('wishlist')
-        ->add(new AuthMiddleware());
-
-       
-
-    $app->post('/wishlist', function ($request, $response) {
-       
-        return $response->withHeader('Location', '/profil')->withStatus(302);
-        })
-        ->setName('wishlist')
-        ->add(new AuthMiddleware());
-
-    $app->get('/offres-postulees', [ProfilController::class, 'offresPostulees'])
-        ->setName('offres-postulees')
-        ->add(new AuthMiddleware());
-        
-  
-    $app->get('/ajout-offre', [OffreController::class, 'ajoute'])
-        ->setName('ajout-offre')
-        ->add(new RoleMiddleware(['admin'],['pilote']))
-        ->add(new AuthMiddleware());
-
-    
-    $app->post('/ajout-offre', [OffreController::class, 'ajoute'])
-        ->add(new RoleMiddleware(['admin'],['pilote']))
-        ->add(new AuthMiddleware());
-
-    $app->post('/offre/supprimer/{id:\d+}', [OffreController::class, 'supprimer'])
-        ->setName('offre-supprimer')
-        ->add(new RoleMiddleware(['admin'],['pilote']))
-        ->add(new AuthMiddleware());
-
-    $app->post('/wishlist/ajouter', function ($request, $response) {
-        if (session_status() === PHP_SESSION_NONE) session_start();
-
-        $data = $request->getParsedBody();
-        $offreId = $data['offre_id'] ?? null;
-
-        if ($offreId) {
-            if (!isset($_SESSION['wishlist'])) $_SESSION['wishlist'] = [];
-
-           
-            $_SESSION['wishlist'][$offreId] = [
-                'id'           => $offreId,
-                'intitule'     => $data['titre'] ?? 'Poste sans titre',
-                'entreprise'   => $data['entreprise'] ?? 'N/A',
-                'localisation' => $data['localisation'] ?? 'Non précisée',
-                'duree'        => 'Stage' // Valeur par défaut
-            ];
-        }
-        return $response->withHeader('Location', '/wishlist')->withStatus(302);
-    })
-        ->setName('wishlist-ajouter')
-        ->add(new AuthMiddleware());
-
-
-    $app->post('/wishlist/supprimer/{id:\d+}', function ($request, $response, $args) {
-        if (session_status() === PHP_SESSION_NONE) session_start();
-    
-    $idOffre = $args['id'];
-
-        if (isset($_SESSION['wishlist'][$idOffre])) {
-            unset($_SESSION['wishlist'][$idOffre]);
-        }
-        return $response->withHeader('Location', '/wishlist')->withStatus(302);
-    })
-        ->setName('wishlist-supprimer')
-        ->add(new AuthMiddleware());
-
-    $app->get('/connexion', [AuthController::class, 'connexion'])->setName('connexion');
-    $app->post('/connexion', [AuthController::class, 'connexion']);
-    $app->get('/deconnexion', [AuthController::class, 'deconnexion'])->setName('deconnexion');
-
     $app->get('/permission', function (Request $request, Response $response) {
         return Twig::fromRequest($request)->render($response, 'permission.html.twig', []);
-    })
-        ->setName('permission');
+    })->setName('permission');
 };
--- src/Domain/Wishlist.php
+++ src/Domain/Wishlist.php
@@ -1,15 +1,9 @@
 <?php
-
 declare(strict_types=1);
-
 namespace App\Domain;
 
 use DateTimeImmutable;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\{Entity, Table, Id, Column, GeneratedValue, ManyToOne, JoinColumn};
 
 #[Entity, Table(name: 'wishlists')]
 class Wishlist
@@ -17,40 +11,20 @@ class Wishlist
     #[Id, Column(type: 'integer'), GeneratedValue(strategy: 'AUTO')]
     private ?int $id = null;
 
-    #[Column(type: 'string', nullable: false)]
-    private string $offreId;
-
-    #[Column(type: 'string', nullable: true)]
-    private ?string $titre = null;
-
-    #[Column(type: 'string', nullable: true)]
-    private ?string $entreprise = null;
-
-    #[Column(type: 'string', nullable: true)]
-    private ?string $localisation = null;
+    // C'est cette relation qui créera la colonne offre_id
+    #[ManyToOne(targetEntity: Offre::class)]
+    #[JoinColumn(name: 'offre_id', referencedColumnName: 'id', nullable: false, onDelete: 'CASCADE')]
+    private Offre $offre;
 
     #[Column(name: 'created_at', type: 'datetimetz_immutable', nullable: false)]
     private DateTimeImmutable $createdAt;
 
-    public function __construct(string $offreId)
+    public function __construct(Offre $offre)
     {
-        $this->offreId   = $offreId;
+        $this->offre = $offre;
         $this->createdAt = new DateTimeImmutable('now');
     }
 
     public function getId(): ?int { return $this->id; }
-
-    public function getOffreId(): string { return $this->offreId; }
-    public function setOffreId(string $offreId): void { $this->offreId = $offreId; }
-
-    public function getTitre(): ?string { return $this->titre; }
-    public function setTitre(?string $titre): void { $this->titre = $titre; }
-
-    public function getEntreprise(): ?string { return $this->entreprise; }
-    public function setEntreprise(?string $entreprise): void { $this->entreprise = $entreprise; }
-
-    public function getLocalisation(): ?string { return $this->localisation; }
-    public function setLocalisation(?string $localisation): void { $this->localisation = $localisation; }
-
-    public function getCreatedAt(): DateTimeImmutable { return $this->createdAt; }
+    public function getOffre(): Offre { return $this->offre; }
 }
